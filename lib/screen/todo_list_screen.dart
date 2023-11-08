554aff9abca44052cb6097e5f55d4fc0323662f8
goodmorningcody <goodmorningcody@gmail.com>
2023-11-09 03:30:46 +0900

    HomeScreen이 TodoListScreen으로 변경됨에 따라 변경된 컴포넌트의 import 변경

--- lib/screen/todo_list_screen.dart
+++ lib/screen/todo_list_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_todo_app/common/enums.dart';
 import 'package:flutter_todo_app/component/todo_add_floating_action_button.dart';
 import 'package:flutter_todo_app/component/todo_list_app_bar.dart';
-import 'package:flutter_todo_app/component/todo_bottom_navigation_bar.dart';
+import 'package:flutter_todo_app/component/todo_list_bottom_navigation_bar.dart';
 
 class TodoListScreen extends StatefulWidget {
   const TodoListScreen({super.key});
