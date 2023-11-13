5b36725265509cf26415839baa1902e1962eae20
cody.return <cody.return@kakaocorp.com>
2023-11-14 04:04:25 +0900

    코드 정리

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'package:flutter_todo_app/data/app_config_repository.dart';
 import 'package:flutter_todo_app/data/todo_repository.dart';
 import 'package:flutter_todo_app/domain/app_config_model.dart';
 import 'package:flutter_todo_app/domain/todo_model.dart';
-import 'package:flutter_todo_app/presentation/component/inherited/app_config_inherited_notifier.dart';
-import 'package:flutter_todo_app/presentation/component/inherited/todo_model_inherited_notifier.dart';
-import 'package:flutter_todo_app/presentation/screen/todo_list_screen.dart';
+import 'package:flutter_todo_app/todo_app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
@@ -24,32 +22,4 @@ void main() async {
       ),
     ),
   );
-}
-
-class TodoApp extends StatelessWidget {
-  final TodoModel todoModel;
-  final AppConfigModel appConfigModel;
-  const TodoApp({
-    super.key,
-    required this.todoModel,
-    required this.appConfigModel,
-  });
-  @override
-  Widget build(BuildContext context) {
-    return AppConfigModelInheritedNotifier(
-      notifier: appConfigModel,
-      child: TodoModelInheritedNotifier(
-        notifier: todoModel,
-        child: MaterialApp(
-          title: 'Todo',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            fontFamily: 'Pretendard',
-            useMaterial3: true,
-          ),
-          home: const TodoListScreen(),
-        ),
-      ),
-    );
-  }
 }
